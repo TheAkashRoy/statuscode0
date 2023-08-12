a013a512cb8c5ec384ae298c9a986e56a96bdf6b
anuHait <anusmitahait67@gmail.com>
2023-08-12 13:48:43 +0530

    new mew

--- frontend/src/components/Landing.jsx
+++ frontend/src/components/Landing.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import {useAuth0} from '@auth0/auth0-react'
-import {loginWithRedirect} from '@auth0/auth0-react'
+import {Link} from 'react-router-dom'
+
 const Landing = () => {
   const {logout} = useAuth0();
   const { user, isAuthenticated, isLoading } = useAuth0();
   const {loginWithRedirect} = useAuth0();
-
   return (
     <div className='p-4'>
    <div className='bg-gray-900 flex p-2 justify-between items-center rounded-full'>
@@ -15,8 +15,8 @@ const Landing = () => {
    {
     isAuthenticated?(
       <div className='flex gap-3'>
-      <button className='text-white font-white text-xl'>Buy</button>
-   <button className='text-white font-white text-xl'>Sell</button>
+      <Link to="/buy"> <button className='text-white font-white text-xl'>Buy</button></Link>
+  <Link to="/sell"> <button className='text-white font-white text-xl'>Sell</button></Link>
       <button onClick={()=>logout()} className='text-xl font-semibold bg-white p-3'>Logout</button>  </div>     ):(
         <button onClick={()=>loginWithRedirect()}
         className=' text-xl font-semibold bg-white p-3'>
--- frontend/src/pages/Sell.jsx
+++ frontend/src/pages/Sell.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import { useAuth0 } from "@auth0/auth0-react";
 
 const Sell = () => {
+  const { user, isAuthenticated, isLoading } = useAuth0();
+  console.log("User",user);
+  if (isLoading) {
+    return <div>Loading ...</div>;
+  }
+console.log("User details",user);
   return (
-    <div>
+    <div className='text-white'>
       sell page
+     {isAuthenticated && (
+        <div>
+          <h2>{user.name}</h2>
+          <p>{user.email}</p>
+        </div>
+      )}
     </div>
   )
 }
