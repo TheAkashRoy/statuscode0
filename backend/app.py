843a6dc1a47c8c7f06b8411aa9c5d4e881faca13
TheAkashRoy <9akash.roy@gmail.com>
2023-08-13 01:37:55 +0530

    com

--- backend/app.py
+++ backend/app.py
@@ -4,14 +4,13 @@ import pymongo
 
 # cySFrQASlOiQf2pt
 
-mongo_URI = "mongodb+srv://9akashroy:cySFrQASlOiQf2@cluster0.hxo7td9.mongodb.net/?retryWrites=true&w=majority"
-
+mongo_URI = "mongodb+srv://9akashroy:AkoxJp6aONZMqpGP@cluster0.pad3mfx.mongodb.net/?retryWrites=true&w=majority"
 app = Flask(__name__)
 CORS(app)
 
 cluster = pymongo.MongoClient(mongo_URI)
-db = cluster["flaskStatus"]
-collection = db['flaskStatus2']
+db = cluster["UserInfo"]
+collection = db['Hubs']
 
 
 @app.route('/')
@@ -58,5 +57,40 @@ def isNew():
     else:
       return "False"
 
+@app.route("/do")
+def dbupdate():
+  collection.insert_one({
+        "_id": 4,
+        "wid0": "avqki59ck1jkq77ola5y0kj7inzyvsan",
+        "wid1": "cfhezx27vbzwrg4xp01omb70fet1vsvf",
+        "wid2": "6xe65w8k3q38kudp5csb5kh9i3f9tu61",
+        "wid3": "5epn8gq6oykoih290ipnxt6l6cue4fxn",
+        "wid4": "l48ntyum9vz02mpwg7o02kxcbvga7slx",
+        "wid5": "2v958j6ml0mfal5i17b6iaupln398yan",
+        "wid6": "uqi59z6i8uovoihhlduh8wqdq8bmukwd",
+        "wid7": "al8cy7bxx1uyhyzgaw4msid442mp8a78",
+        "wid8": "jgmuom2fqw8lsfx0zgueo98sa4xmaj51",
+        "wid9": "7b92ipdnjteliop98eqkt78fzk5r6kz1",
+  })
+  return "ok"
 
-app.run(host='0.0.0.0', port=81)
+# import string
+# import random
+ 
+# N = 32
+ 
+# res = ''.join(random.choices(string.ascii_lowercase +
+#                              string.digits, k=N))
+# print("{",end="")
+# for i in range(10):
+#     print('"wid'+str(i)+'":', end = " ")
+#     res = ''.join(random.choices(string.ascii_lowercase + string.digits, k=N))
+#     print('"',end="")
+#     print(res, end = "")
+#     print('"',end="")
+#     print(",")
+# print("}")
+     
+
+if __name__ == "__main__":
+    app.run(debug=True)
